d023ce66dd3dcb209c86bfc6412a64963809f1c2
mecom8489 <mecom8489@gmail.com>
2026-07-10 17:58:41 +0530

    feat: add responsive Footer component with navigation and contact links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -133,8 +133,7 @@ export default function Footer({ setActiveTab }) {
                 {[
                   { icon: 'M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z', label: 'Facebook' },
                   { icon: 'M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z M17.5 6.5h.01', label: 'Instagram', isInsta: true },
-                  { icon: 'M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z M2 9h4v12H2z', label: 'LinkedIn' },
-                  { icon: 'M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z', label: 'YouTube' }
+                 
                 ].map((social, i) => (
                   <a
                     key={i}
